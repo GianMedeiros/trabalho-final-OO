3dbb39765712f722902c1973beace913e4004b4b
GianMedeiros <gianmedeiros14@gmail.com>
2021-10-13 22:28:13 -0300

    :hammer: adicionei o arquivo main e a classe pessoa

---
+++ src/app/main.java
@@ -0,0 +1,17 @@
+package app;
+
+import javax.swing.JOptionPane;
+
+import classes.Pessoa;
+
+public class Republica {
+
+    static Pessoa[] pessoas = new Pessoas[0];       //referencia representa a associacao MULTIPLA entre Republica e pessoa
+
+    public static void main(String[] args) {
+
+        
+
+    }
+
+}
---
+++ src/classes/pessoa.java
@@ -0,0 +1,24 @@
+package classes;
+
+public class Pessoa {
+    private String nome;
+    private String email;
+    private float valor;
+
+    public cadastrarPessoa(nome, email, valor) {
+        // Função para cadastrar pessoa
+        this.nome = nome;
+        this.email = email;
+        this.valor = valor;
+    }
+
+    public void gastosPorPessoa() {
+        // Função  para criar o objeto
+
+    }
+
+    public Boolean alterarArquivoPessoas(modo) {
+        // função que ira salvar ou remover pessoas no arquivo pessoas.txt
+
+    }
+}
